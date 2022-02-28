41c959bf7cc9e70be1f1655129717261e9e6c1e1
kenya amilcar <amilcarkenya@gmail.com>
2022-02-28 05:03:06 -0500

    commit tree 37f13629143dce2b6d6b42950bdba41d1e73e4aa

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)
+console.log(random);
+
+let num3 = 32
+let num4 = 8
+const mod = num3 / num4 
+
+let max = Math.max(4, 8, 20, 7,3)
+console.log(max);
